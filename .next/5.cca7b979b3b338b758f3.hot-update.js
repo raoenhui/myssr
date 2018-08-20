3f74e872b1fc822bc9fe98692d8bab4b07795821
raoenhui <raoenhui@jd.com>
2018-08-20 18:08:43 +0800

    first commit

---
+++ .next/5.cca7b979b3b338b758f3.hot-update.js
@@ -0,0 +1,2 @@
+webpackHotUpdate(5,[])
+//# sourceMappingURL=5.cca7b979b3b338b758f3.hot-update.js.map
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import "../less/styles.less"
+
+export default () => (
+   <div className="example">
+     Hello World!
+     <img src="/static/my-image.png" alt="my image" />
+   </div>
+)
